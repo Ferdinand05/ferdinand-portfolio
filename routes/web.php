92cc3166cc855422109e47d92eb3a37e4de480d0
Ferdinand05 <fkoryanto@gmail.com>
2024-03-18 10:11:21 +0700

    add:login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
+Route::get('login', [LoginController::class, 'index'])->name('login');
+Route::post('login', [LoginController::class, 'store'])->name('login.store');
+Route::get('logout', [LoginController::class, 'logout'])->name('logout');
